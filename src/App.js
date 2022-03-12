8575d06cfae265b70ff5acbfb133a1874d789a0c
hendridg <hendridg@gmail.com>
2022-03-12 15:46:38 -0400

    Create react applications

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello from App</h1>
+    </div>
+  );
+}
+
+export default App;
