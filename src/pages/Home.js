fe7b87a71bb0fd4842e4541a9b816a8bf721e390
hendridg <hendridg@gmail.com>
2022-03-14 16:39:50 -0400

    Add structure in Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import About from '../components/About';
+import Contact from '../components/Contact';
+import Heading from '../components/Heading';
+import Works from '../components/Works';
 
 const Home = () => (
-  <div>Home</div>
+  <div
+    style={{
+      margin: '2em',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      gap: '8rem',
+    }}
+  >
+    <Heading />
+    <Works />
+    <About />
+    <Contact />
+  </div>
 );
 
 export default Home;
