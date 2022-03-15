f00c978ff2fb2ee1db57bc8e7ece89c1d1df4bf0
hendridg <hendridg@gmail.com>
2022-03-15 15:56:32 -0400

    Create recentWorks const

---
+++ src/recentWorks.js
@@ -0,0 +1,41 @@
+const recentWorks = [
+  {
+    flick: '/assets/cards/screenshot-01.png',
+    alt: 'covid app',
+    title: 'COVID-19 Statistic',
+    urlLinkLive: 'https://mystifying-varahamihira-cda443.netlify.app/',
+    urlLinkGitHub: 'https://github.com/hendrig/',
+    tools: ['react', 'react-redux', 'react-router', 'styled-components'],
+    leftRender: false,
+    description: `Is a web app using https://covid19tracking.narrativa.com/ API, with
+            responsive design you can filter the data by day, and select the
+            country to details information, the principal tools was React,
+            Redux, Redux-Toolkits, React-Router and Styled-Components.`,
+  },
+  {
+    flick: '/assets/cards/react-screenshot-01.png',
+    alt: 'space travelers app',
+    title: "🚀 Space Travelers 'HUB",
+    urlLinkLive: 'https://upbeat-perlman-1981ee.netlify.app/',
+    urlLinkGitHub: 'https://github.com/hendrig/',
+    tools: ['react', 'react-redux', 'react-router', 'styled-components'],
+    leftRender: true,
+    description: `A nice web page was made it using https://api.spacexdata.com/v3 API, 
+            the principal tools React, Redux and Styled-Components, 
+            you can navigate and Reserve your trips to space 🚀, 
+            finally in the "My Profile" section have the resume of your reservations.`,
+  },
+  {
+    flick: '/assets/cards/screenshot_3.png',
+    alt: 'creative app',
+    title: 'Creative Commons Global',
+    urlLinkLive: 'https://hendridg.github.io/capstone-module-1/',
+    urlLinkGitHub: 'https://github.com/hendrig/',
+    tools: ['responsive', 'html', 'CSS', 'JavaScript'],
+    leftRender: false,
+    description: `Web page made it with HTML, CSS, and JavaScript, 
+            responsive design, and beautiful navigations`,
+  },
+];
+
+export default recentWorks;
