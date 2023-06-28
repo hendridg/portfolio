9157d96120d96476607b74504ea67593013c5216
hendridg <hendridg@gmail.com>
2023-06-28 12:12:48 -0400

    add comments on blog page

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import UnderConstruction from '../components/UnderConstruction';
 
+/**
+ * Blog page
+ * @returns Blog page
+ */
+
 const Blog = () => <UnderConstruction />;
 
 export default Blog;
